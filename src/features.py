de585c57870cb90efc604c5f90a6c43fcc9e605f
youssefkurdi03 <youssef.kurdi03@gmail.com>
2026-01-10 23:48:47 +0300

    Force update to fix red build

--- src/features.py
+++ src/features.py
@@ -1,10 +1,6 @@
 import hashlib
 
 def get_hash_bucket(input_string, num_buckets=1000):
-    """
-    Hashes an input string into a fixed number of buckets.
-    Required for feature engineering consistency in MLOps.
-    """
     if not input_string:
         return 0
     hash_value = hashlib.md5(str(input_string).encode()).hexdigest()
